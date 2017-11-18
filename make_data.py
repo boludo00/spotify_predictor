991762d30b6a5f2e20384cabf896d741e5c158cb
Giancarlo Perrone <gperrone@westhealth.org>
2017-11-18 09:48:10 -0800

    refactored code and cleaned overall implementation

--- make_data.py
+++ make_data.py
@@ -7,9 +7,11 @@ import spotipy.util as util
 import pandas as pd
 from sklearn.utils import shuffle
 import coloredlogs, logging
+import time
+
 
 logger = logging.getLogger(__name__)
-coloredlogs.install(level='INFO', logger=logger)
+coloredlogs.install(level='DEBUG', logger=logger)
 
 
 # read credentials and set them
@@ -27,56 +29,70 @@ token = util.prompt_for_user_token("Giancarlo Perrone",
 sp = spotipy.Spotify(auth=token)
 
 # do 1000 at a time
-songs = shuffle(pd.read_csv("songdata.csv"))[['song', 'artist']][:1000]
+songs = shuffle(pd.read_csv("songdata.csv"))[['song', 'artist']][:5000]
 
 ids = []
-song_count = 0
+feats = []
+song_count = 1
+pkl_ids = pd.read_pickle("ids.pkl")
+pkl_feats = pd.read_pickle("features.pkl")
 # use this function here to aggregate track ids
-for s in songs.iterrows():
+for i, s in enumerate(songs.iterrows()):
+    # skip duplicates
+    if s[1].song in map(lambda s: s[2], pkl_ids):
+        logger.warning("Duplicate [%s] found!" % s[1].song)
+        continue
     try:
-        track_id = sp.search(s[1].song, limit=1, offset=0, type='track', market=None)["tracks"]['items'][0]["id"]
-        artist_id = sp.search(s[1].song, limit=1, offset=0, type='track', market=None)["tracks"]['items'][0]["artists"][0]["id"]
-        album_id = sp.search(s[1].song, limit=1, offset=0, type='track', market=None)["tracks"]['items'][0]["album"]["id"]
+        result = sp.search(s[1].song, limit=1, offset=0, type='track', market=None)
+        logger.debug(result["tracks"]["items"])
+        track_id = result["tracks"]['items'][0]["id"]
+        artist_id = result["tracks"]['items'][0]["artists"][0]["id"]
+        album_id = result["tracks"]['items'][0]["album"]["id"]
+
+        logger.info("[%d] Processing %s : %s --- track_id: %s artist_id: %s album_id: %s" % (song_count, s[1].artist, s[1].song, track_id, artist_id, album_id))
+
         # append 4-element tuple (653FhwnB8YXSwqbuPn8eB4, Mac Miller, BDE Bonus, 4LLpKhyESsyAXpc4laK94U, 0ETFjACtuP2ADo6LFhL6HN)
         ids.append((track_id, s[1].artist, s[1].song, artist_id, album_id))
-        logger.debug("Processing %s : %s with track id %s artist id %s album id %s" % (s[1].artist, s[1].song, track_id, artist_id, album_id))
-        logger.info("[%d] Processing %s : %s" % (song_count, s[1].artist, s[1].song))
-        song_count += 1
+        ft = sp.audio_features(tracks=[track_id])
+        logger.debug("\n%s Feature: %s" % (s[1].song, ft))
+        feats.append(ft[0])
+        popl = sp.track(track_id)["popularity"]
+        art_followers = sp.artist(artist_id)
+        song_date = sp.album(album_id)["release_date"]
+        logger.debug(feats[song_count - 1])
+        feats[song_count - 1].update({"artist": s[1].artist, "song": s[1].song, "popularity": popl, "artist_followers": art_followers["followers"]["total"], "release_date": song_date})        
+        
         # create pkl here, just in case 
-        pd.to_pickle(ids, "ids.pkl")
+        pd.to_pickle(ids + pkl_ids, "ids.pkl")
+        pd.to_pickle(feats + pkl_feats, "features.pkl")
+        song_count += 1
     except Exception as e:
-        logger.warning("%s --- on %s" % (type(e), s[1].song))
+        logger.error("%s, %s --- on %s" % (type(e), e, s[1].song))
+        # time.sleep(3)
 
-# create pkl here, just in case 
-# pd.to_pickle(ids, "ids.pkl")
-logger.info("Successfully gathered info for %d tracks" % song_count)
-
-feats = []
-feat_count = 0
-for i, d in enumerate(ids):
-    try:
-        feats.append(sp.audio_features(tracks=[d[0]]))
-        popl = sp.track(d[0])["popularity"]
-        art_followers = sp.artist(d[3])
-        song_date = sp.album(d[4])["release_date"]
-        feats[i][0].update({"artist": d[1], "song": d[2], "popularity": popl, "artist_followers": art_followers["followers"]["total"], "release_date": song_date})
-        logger.info("[%d] Added features for %s " % (feat_count, d[2]))
-        feat_count += 1
-    except:
-        logger.warning("%s did not yield audio features!!!" % d[2])
-
-logger.info("Successfully added features for %d track ids" % feat_count)
-
-# flatten the inner list in the object
-feats = map(lambda x: x[0], feats)
 
 with open("features.json", "w") as f:
-    json.dump(feats, f, indent=4)
+    json.dump(pd.read_pickle("features.pkl") + feats, f, indent=4)
 
-df = pd.DataFrame(data=feats)
-df.drop(['analysis_url', 'track_href', 'type', 'uri'], axis=1, inplace=True)
 
-# re-order columns for visibility
-df = df[['artist', 'song', 'artist_followers', 'release_date', 'acousticness', 'danceability', 'duration_ms', 'energy',
-        'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'speechiness', 'tempo', 'time_signature', 'valence', 'popularity']]
-df.to_csv("data1.csv")
+logger.info("Successfully added features for %d track ids (see features.json or .pkl file)" % (song_count - 1))
+
+
+# feats = []
+# feat_count = 0
+# for i, d in enumerate(ids):
+#     try:
+#         feats.append(sp.audio_features(tracks=[d[0]]))
+#         popl = sp.track(d[0])["popularity"]
+#         art_followers = sp.artist(d[3])
+#         song_date = sp.album(d[4])["release_date"]
+#         feats[i][0].update({"artist": d[1], "song": d[2], "popularity": popl, "artist_followers": art_followers["followers"]["total"], "release_date": song_date})
+#         logger.info("[%d] Added features for %s " % (feat_count, d[2]))
+#         feat_count += 1
+#     except:
+#         logger.warning("%s did not yield audio features!!!" % d[2])
+# with open("features.json") as f:
+#     real_data = json.loads(f.read())
+
+
+
